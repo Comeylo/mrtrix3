76103ef747f9d0e76c687ca6c6f944fca054b19b
come le breton <clebreto@avalon.inria.fr>
2019-10-10 15:45:35 +0200

    exec_version files are generated during the configuration step, based on git values

--- src/exec_version.cpp
+++
@@ -1,7 +0,0 @@
-
-namespace MR {
-  namespace App {
-    extern const char* executable_uses_mrtrix_version;
-    void set_executable_uses_mrtrix_version () { executable_uses_mrtrix_version = "3.0_RC3_latest-5-g81036fcc"; }
-  }
-}
